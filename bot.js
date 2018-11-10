1662cd20fd64660674f0b552722b90975dd14dfa
faresgin <44904357+faresgin@users.noreply.github.com>
2018-11-10 02:00:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '.'
+
 client.on('ready', () => {
-  client.user.setGame(':two_hearts: بسم اللة الرحمن الرحيم :two_hearts:','https://www.twitch.tv/TEST-Broadcast%27);
-  console.log('---------------');
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Five Stars`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
   console.log(' Bot Is Online')
-  console.log('---------------')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
-client.login('NTA5MTE4MTA2NzcwMDc5NzQ1.DsdNQA.NeciDsNNbANOHWgYCJiZvmslGcU');
+client.login(process.env.BOT_TOKEN);
