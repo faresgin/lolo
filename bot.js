fbd7cbd120752ad3f26872a6babe9666845e5229
faresgin <44904357+faresgin@users.noreply.github.com>
2018-11-13 01:10:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -206,4 +206,14 @@ client.on("message", message => {
    }
    });
 
+
+
+client.on('message', message=> {
+    if (message.author.bot) return;
+    if (message.isMentioned(client.user))
+    {
+    message.reply(" :heart: هلا حبي اتمني ان اكون عجبتك ايش  تريد؟  ");
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
